255182f11cd80f93674a290693c18bb6e0485880
mehrdad-kj <mehrdadk.jourabi@gmail.com>
2022-11-26 08:43:35 +0330

    first commit

---
+++ src/Components/BottomChart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BottomChart() {
+  return (
+    <div className='bottom-chart'>BottomChart</div>
+  )
+}
+
+export default BottomChart
---
+++ src/Components/RightChart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RightChart() {
+  return (
+    <div className='right-chart'>RightChart</div>
+  )
+}
+
+export default RightChart
